b2194bd04cf9330ad78b08a66fc82b2ff73b15a1
Mohammed Junaid <junaid7mohamed@gmail.com>
2025-01-22 10:34:15 +0530

    revert base url fix

--- src/config/axios.config.js
+++ src/config/axios.config.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: `https://memories-be-xi.vercel.app`,
+    baseURL: `${import.meta.env.VITE_SERVER_BASE_URL}`,
     withCredentials: true,
 });
 
